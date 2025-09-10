f53ae7e306ee1c67a5aed505cadc03c3a565a30b
unknown <david.barbosa-g@labinfo.is.escuelaing.edu.co>
2025-09-10 10:54:53 -0500

    cliente y back corriendo pero las funciones aun no sirven

--- src/main/java/backend/ReflexiveBackend.java
+++ src/main/java/backend/ReflexiveBackend.java
@@ -1,4 +1,109 @@
 package backend;
 
+import java.net.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
 public class ReflexiveBackend {
+
+    private static LinkedList<String> linkedList = new LinkedList<>();
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(45000);
+            System.out.println("Backend escuchando en puerto 45000...");
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 45000.");
+            System.exit(1);
+        }
+        Socket clientSocket = null;
+
+        boolean running = true;
+
+        while (running) {
+            try {
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine = "{\"error\":\"Sin respuesta\"}";
+
+            String path = null;
+            boolean firstline = true;
+            URI requri = null;
+
+            while ((inputLine = in.readLine()) != null) {
+                if (firstline) {
+                    requri = new URI(inputLine.split(" ")[1]);
+                    System.out.println("Path: " + requri.getPath());
+                    firstline = false;
+                }
+                System.out.println("Received: " + inputLine);
+                if (!in.ready()) {
+                    break;
+                }
+            }
+
+            String query = requri.getQuery();
+            if (query != null) {
+                if (query.contains("add")) {
+                    outputLine = appendValue(requri);
+                } else if (query.contains("list")){
+                    outputLine = listValues();
+                } else if (query.contains("clear")){
+                    outputLine = deleteList();
+                } else if (query.contains("stats")){
+                    outputLine = stats();
+                }
+            } else {
+                outputLine = "Consulta no valida";
+            }
+
+            out.println("HTTP/1.1 200 OK");
+            out.println("Content-Type: application/json");
+            out.println("Connection: close");
+            out.println("Content-Length: " + outputLine.length());
+            out.println();
+            out.println(outputLine);
+            out.flush();
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        serverSocket.close();
+    }
+
+    public static String appendValue(URI requri){
+        String query = requri.getQuery();
+        String numero = URLDecoder.decode(query.substring(6), StandardCharsets.UTF_8);
+        linkedList.add(numero);
+        return "{\"status\":\"" + numero + "\"}";
+    }
+
+    public static String listValues(){
+        StringBuilder sb = new StringBuilder("{\"values\":[");
+        for (String s : linkedList) {
+            sb.append("\"").append(s).append("\"");
+
+        }
+        sb.append("]}");
+
+        return sb.toString();
+    }
+
+
+    public static String deleteList(){
+        linkedList.clear();
+        return "{\"list_cleared\":\" }";
+    }
+
+    public static String stats(){
+
+        return "";
+    }
 }
---
+++ src/main/java/facade/Facade.java
@@ -0,0 +1,94 @@
+package facade;
+
+import java.io.*;
+import java.net.*;
+
+public class Facade {
+
+    public static void main(String[] args) throws Exception {
+        new Facade();
+    }
+
+    public Facade() throws Exception {
+        ServerSocket serverSocket = new ServerSocket(35000);
+        System.out.println("Facade escuchando en puerto 35000...");
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            OutputStream out = clientSocket.getOutputStream();
+
+            String line = in.readLine();
+            if (line == null) continue;
+
+            if (line.contains("/cliente")) {
+                String html = getClientHtml();
+                sendResponse(out, "text/html", html);
+            } else if (line.contains("/")){
+                String option = line.split("/")[1];
+                String backendResp = callBackend(option);
+                sendResponse(out, "application/json", backendResp);
+            }
+
+            clientSocket.close();
+        }
+    }
+
+    private static String callBackend(String option) throws Exception {
+        URL url = new URL("http://localhost:45000/" + option);
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+        return br.readLine();
+    }
+
+    private static void sendResponse(OutputStream out, String contentType, String body) throws IOException {
+        String response = "HTTP/1.1 200 OK\r\n" +
+                "Content-Type: " + contentType + "\r\n" +
+                "\r\n" +
+                body;
+        out.write(response.getBytes());
+    }
+
+
+    private static String getClientHtml() {
+        return "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "    <title>Parcial 1</title>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "\n" +
+                "<h1>Interactuar con linkedList</h1>\n" +
+                "<input type=\"text\" id=\"num\" placeholder=\"añadir a cola...\">\n" +
+                "<button onclick=\"sendCommand()\">Enviar</button>\n" +
+                "\n" +
+                "<pre id=\"response\"></pre>\n" +
+                "\n" +
+                "<script>\n" +
+                "    function sendCommand(){\n" +
+                "        let url = \"/add?x=\" + document.getElementById(\"num\").value;\n" +
+                "        fetch(url)\n" +
+                "            .then(x => x.text())\n" +
+                "            .then(y => document.getElementById(\"response\").innerHTML = y);\n" +
+                "    }\n" +
+                "</script>\n" +
+
+                "<button onclick=\"sendCommand()\">list</button>\n" +
+                "\n" +
+                "<pre id=\"response\"></pre>\n" +
+                "\n" +
+                "<script>\n" +
+                "    function sendCommand(){\n" +
+                "        let url = \"/list\"" +
+                "        fetch(url)\n" +
+                "            .then(x => x.text())\n" +
+                "            .then(y => document.getElementById(\"response\").innerHTML = y);\n" +
+                "    }\n" +
+                "</script>\n" +
+
+
+                "</body>\n" +
+                "</html>";
+    }
+}
